2c0a4d4d57e3ff82860f41193eb677fb4a19b7dc
MLopusiewicz <marcin.lopusiewicz@outlook.com>
2021-05-22 02:23:31 +0200

    skrypt kalibrujący

---
+++ Assets/StartMove.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMove : MonoBehaviour {
+	public Transform room, cam;
+	private IEnumerator Start() {
+		yield return new WaitForSeconds(2f);
+		room.transform.position -= cam.transform.position - transform.position;
+		//room.rotation = transform.rotation;
+	}
+}
